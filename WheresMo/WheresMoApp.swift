8deb0d086cd4cf26ebf376597ade6f219532d13c
npbassett <bassett.neil@gmail.com>
2022-11-11 16:22:21 -0700

    Initial Commit

---
+++ WheresMo/WheresMoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WheresMoApp.swift
+//  WheresMo
+//
+//  Created by Neil Bassett on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct WheresMoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
